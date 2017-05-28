9c043c13d1c19085686bb1b60abea8a013229280
smitherzz <thimbletemail@gmail.com>
2017-05-28 18:30:28 -0400

    Update mario.c

--- wk1/mario.c
+++ wk1/mario.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 void verifyLevels(int i);
-void pickANumber(void);
+void startGame(void);
 void printPyramid(int levels);
+void printHashes(int row);
+void printSpaces(int height);
 
 int main(void)
 {
-    pickANumber();
+    startGame();
 }
 
 void verifyLevels(int i)
@@ -15,7 +17,7 @@ void verifyLevels(int i)
     if (i < 1 || i > 23) 
     {
         printf("Try again\n");
-        pickANumber();
+        startGame();
     } 
     else 
     {
@@ -24,7 +26,7 @@ void verifyLevels(int i)
     }
 }
 
-void pickANumber(void)
+void startGame(void)
 {
     printf("Pick a number 1 - 23\n");
     int levels = get_int();
@@ -36,17 +38,25 @@ void printPyramid(int levels) {
     int row = 1;
     
     for (int i = 1; i <= levels; i++) {
-        for (int x = 1; x < height; x++) {
-            printf(" ");
-        }
+        printSpaces(height);
         
-        for (int x = 1; x <= row; x ++) {
-            printf("#");
-        }
+        printHashes(row);
 
         printf("\n");
         
         height--;
         row++;
+    }
+}
+
+void printHashes(int row) {
+    for (int x = 1; x <= row; x++) {
+        printf("#");
+    }
+}
+
+void printSpaces(int height) {
+    for (int x = 1; x < height; x++) {
+        printf(" ");
     }
 }
